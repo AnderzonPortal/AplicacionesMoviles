78ad7266d67716e0be627e5a596cb738c770a8dd
anderzon <anderzonportales@gmail.com>
2025-05-20 20:30:38 -0500

    Eliminar todas las carpetas del repositorio remoto

--- S4_AM_Quizz/app/src/main/java/com/example/s4_quizz/Pregunta.kt
+++
@@ -1,7 +0,0 @@
-package com.example.s4_quizz
-
-data class Pregunta(
-    val texto: String,
-    val opciones: List<String>,
-    val respuestaCorrecta: Int // índice de la respuesta correcta (0 a 3)
-)
